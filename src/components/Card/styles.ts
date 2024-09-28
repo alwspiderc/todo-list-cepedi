5eff51e99211bd10f1ce80b1ba475eff02053058
alwspiderc <alwesterc@gmail.com>
2024-09-28 14:12:40 -0300

    first commit

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	container: {
+		backgroundColor: 'white',
+		padding: 20,
+		borderRadius: 10,
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.1,
+		shadowRadius: 10,
+		elevation: 5
+	}
+});
