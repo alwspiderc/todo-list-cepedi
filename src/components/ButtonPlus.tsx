d56176ce0d59988c3ece8e725be0e53664d302c2
alwspiderc <alwesterc@gmail.com>
2024-09-30 10:57:07 -0300

    fix: ajustes ButtonPlus

--- src/components/ButtonPlus.tsx
+++ src/components/ButtonPlus.tsx
@@ -1,9 +1,15 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export function ButtonPlus() {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>+</Text>
+			<TouchableOpacity
+				style={styles.content}
+				onPress={() => console.log('criar task')}
+			>
+				<Text style={styles.text}>+</Text>
+			</TouchableOpacity>
 		</View>
 	);
 }
@@ -14,16 +20,26 @@ const styles = StyleSheet.create({
 		height: 60,
 		backgroundColor: '#011F26',
 		borderRadius: 30,
-		justifyContent: 'center',
-		alignItems: 'center',
 		zIndex: 1,
-		top: '80%',
+		top: '85%',
 		left: '80%',
 		shadowColor: 'black',
-		shadowOffset: { width: 0, height: 2 },
+		shadowOffset: {
+			width: 0,
+			height: 2
+		},
 		shadowOpacity: 0.1,
 		shadowRadius: 5,
 		elevation: 5
+	},
+	content: {
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderWidth: 2,
+		borderColor: '#F5F4F8'
 	},
 	text: {
 		color: 'white',
