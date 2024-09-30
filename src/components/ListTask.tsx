ce3b388bed636bc59f1328f66870137c89c35f4f
alwspiderc <alwesterc@gmail.com>
2024-09-30 12:35:35 -0300

    fix: json tasks

--- src/components/ListTask.tsx
+++ src/components/ListTask.tsx
@@ -1,25 +1,8 @@
 import { ReactElement } from 'react';
 import { FlatList } from 'react-native';
-
+import data from '../mock/tasks.json';
 import { Card } from './Card';
 
-const data = [
-	{ name: 'Task 1', checked: false },
-	{ name: 'Task 2', checked: true },
-	{ name: 'Task 3', checked: true },
-	{ name: 'Task 4', checked: true },
-	{ name: 'Task 5', checked: true },
-	{ name: 'Task 6', checked: true },
-	{ name: 'Task 7', checked: true },
-	{ name: 'Task 8', checked: true },
-	{ name: 'Task 9', checked: true },
-	{ name: 'Task 10', checked: true },
-	{ name: 'Task 11', checked: true },
-	{ name: 'Task 12', checked: true },
-	{ name: 'Task 13', checked: true },
-	{ name: 'Task 14', checked: true },
-	{ name: 'Task 15', checked: true }
-];
 export function ListTask({
 	ListHeaderComponent
 }: {
